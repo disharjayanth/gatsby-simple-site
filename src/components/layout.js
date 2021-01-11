d6f70742615d583594b6eeef45a9341d8c82f655
disharjayantha <dishuj15@gmail.com>
2021-01-11 16:54:20 +0530

    header margin bottom increased

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ const ListLink = (props) => (
 export default function Layout({ children }) {
   return (
     <div className="my-10 mx-10 sm:my-12 sm:mx-auto w-3/4 sm:w-1/2 px-4">
-      <header className="mb-6">
+      <header className="mb-10">
         <Link to="/">
           <h3 className="inline">Myself</h3>
         </Link>
